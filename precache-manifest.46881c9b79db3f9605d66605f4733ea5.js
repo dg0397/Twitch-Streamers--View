addd58abfae7a018bbd57a51699d5197d2eb1dcc
dg0397 <rgg0397@gmail.com>
2020-10-01 16:57:44 -0400

    Updates

---
+++ precache-manifest.46881c9b79db3f9605d66605f4733ea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7575c4934d90e7032cf55c04e5858fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d37208d777e1fa380e1",
+    "url": "/static/css/main.2d0ec9d6.chunk.css"
+  },
+  {
+    "revision": "53a11b8c6dca9a496540",
+    "url": "/static/js/2.27a48985.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27a48985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d37208d777e1fa380e1",
+    "url": "/static/js/main.f1805662.chunk.js"
+  },
+  {
+    "revision": "6604eadc9f66d9abec93",
+    "url": "/static/js/runtime-main.4bcec1d1.js"
+  }
+]);
